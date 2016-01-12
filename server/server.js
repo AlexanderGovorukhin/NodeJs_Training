a8d34aadda5444a9466f081631367f898da34165
Alexander Govorukhin <alexander.govorukhin@itechart-group.com>
2016-01-12 14:50:47 +0300

    lesson_15 (echo-server)

--- server/server.js
+++ server/server.js
@@ -1,19 +1,29 @@
-var  http = require('http');
+//  http://127.0.0.1:1337/echo?message=Hello -> Hello
 
-var server = new http.Server();  // http.Server -> net.Server -> EventEmitter
+var  http = require('http');
+var url = require('url');
+
+var server = new http.Server(function(req, res){
+
+    //console.log(req.headers);
+
+    //console.log(req.method, req.url);
+    var urlParsed = url.parse(req.url, true);
+    //console.log(urlParsed);
+
+    if (urlParsed.pathname == '/echo' && urlParsed.query.message){
+        res.setHeader('Cache-control', 'no-cache');
+
+        //res.statusCode = 200;  // OK
+        res.end(urlParsed.query.message);
+    } else{
+        res.statusCode = 404;  // Not Found
+        res.end("Page not found");
+    }
+});
+
+// http.Server -> net.Server -> EventEmitter
 
 server.listen(1337,'127.0.0.1');
 
 
-var counter = 0;
-
-
-var emit = server.emit;
-server.emit = function(event /*, arg1,arg2,...   */){
-    console.log(event);
-    emit.apply(server, arguments);
-};
-
-server.on('request', function(req,res){
-    res.end("Привет, мир!" + ++counter);
-})
