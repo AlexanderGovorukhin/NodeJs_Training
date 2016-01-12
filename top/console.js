95615606fb7c4551fcbf8d7259b8d6747aa4dd4b
Alexander Govorukhin <alexander.govorukhin@itechart-group.com>
2016-01-12 14:48:39 +0300

    lesson_12 (Error)

--- top/console.js
+++
@@ -1,5 +0,0 @@
-// NO console.debug
-
-console.log("Log"); // = info (out)
-
-console.error("Error");  // = warn (err)
--- top/format.js
+++
@@ -1,5 +0,0 @@
-var util = require('util');
-
-var str = util.format("My %s %d %j", "string", "1", {test:"obj"});
-
-console.log(str);
