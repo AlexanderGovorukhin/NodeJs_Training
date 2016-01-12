1b8a853dda3b1412b01fa1ff2bc60083d511fbcd
Alexander Govorukhin <alexander.govorukhin@itechart-group.com>
2016-01-12 14:49:19 +0300

    lesson_13 (Events)

---
+++ top/ee.js
@@ -0,0 +1,23 @@
+var EventEmitter = require('events').EventEmitter;
+
+var server = new EventEmitter;
+
+server.on('request', function(request){
+    request.approved = true;
+});
+
+
+server.on('request', function(request){
+    console.log(request);
+});
+
+server.emit('request', {from:"Клиент"});
+
+server.emit('request', {from:"Ещё Клиент"});
+
+
+server.on('error', function(err){
+   //
+});
+
+server.emit('error', new Error("Серверная ошибка"));
