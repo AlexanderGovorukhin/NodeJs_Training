4b525531726e23874f90249d8489c4b2be26769d
Alexander Govorukhin <alexander.govorukhin@itechart-group.com>
2016-01-12 15:03:39 +0300

    lesson_35 (mongoose)

--- chat/createDb.js
+++ chat/createDb.js
@@ -1,25 +1,16 @@
-var MongoClient = require('mongodb').MongoClient
-    , format = require('util').format;
+var User = require('models/user').User;
 
+var user = new User({
+    username: "Tester",
+    password: "secret",
 
-// Connection URL
-var url = 'mongodb://localhost:27017/chat';
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
+});
 
-    var collection = db.collection('test_insert');
+user.save(function(err, user, affected){
+   if (err) throw err;
 
-    collection.remove({}, function (err, result) {
-        if (err) throw err;
-
-        collection.insert({a: 2}, function (err, docs) {
-            var cursor = collection.find({a: 2});
-            cursor.toArray(function (err, result) {
-                console.dir(result);
-                db.close();
-            });
-        });
+    User.findOne({username: "Tester"}, function(err, tester){
+        console.log(tester);
     });
 });
 
