848078ef91ecd6e5daccd9a14c86054bb3461ef2
Alexander Govorukhin <alexander.govorukhin@itechart-group.com>
2016-01-12 14:42:23 +0300

    lesson_6 (Working with Modules)

--- start/user/index.js
+++ start/user/index.js
@@ -1,4 +1,6 @@
-var phrases = require('./ru');
+//module.exports = exports = this
+var db = require('db');
+var log = require('logger')(module);
 
 function User(name){
     this.name = name;
@@ -6,11 +8,15 @@ function User(name){
 
 User.prototype.hello = function(who){
     //...
-    console.log(phrases.Hello + ", " + who.name);
+    log(db.getPhrase("Hello") + ", " + who.name);
 }
 //...
 
-console.log("user.js is required!");
+
+//console.log("user.js is required!");
 
 //exports
-exports.User = User;
+//exports.User = User;
+module.exports = User;
+
+//console.log(module);
