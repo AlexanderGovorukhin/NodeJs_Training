54b24df98c316b55146cacb844f3498061824108
Alexander Govorukhin <alexander.govorukhin@itechart-group.com>
2016-01-12 14:51:14 +0300

    lesson_17 (supervisor)

--- server/server.js
+++ server/server.js
@@ -3,26 +3,23 @@
 var  http = require('http');
 var url = require('url');
 
+
 var server = new http.Server(function(req, res){
 
-    //console.log(req.headers);
-
-    //console.log(req.method, req.url);
     var urlParsed = url.parse(req.url, true);
-    //console.log(urlParsed);
 
     if (urlParsed.pathname == '/echo' && urlParsed.query.message){
-        res.setHeader('Cache-control', 'no-cache');
-
-        //res.statusCode = 200;  // OK
         res.end(urlParsed.query.message);
-    } else{
-        res.statusCode = 404;  // Not Found
-        res.end("Page not found");
+        return;
     }
+
+    res.statusCode = 404;  // Not Found
+    res.end("Page not found");
 });
 
 // http.Server -> net.Server -> EventEmitter
+
+
 
 server.listen(1337,'127.0.0.1');
 
